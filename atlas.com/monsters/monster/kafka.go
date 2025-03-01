41791d00566b76fe82a7017060d234b4367a6b1d
Chronicle20 <a.chronicle.20@gmail.com>
2025-03-01 12:15:08 -0500

    Add event status for monster taking damage. This will be used to show HP bar.

--- atlas.com/monsters/monster/kafka.go
+++ atlas.com/monsters/monster/kafka.go
@@ -8,6 +8,7 @@ const (
 	EventMonsterStatusDestroyed    = "DESTROYED"
 	EventMonsterStatusStartControl = "START_CONTROL"
 	EventMonsterStatusStopControl  = "STOP_CONTROL"
+	EventMonsterStatusDamaged      = "DAMAGED"
 	EventMonsterStatusKilled       = "KILLED"
 
 	MovementTypeNormal        = "NORMAL"
@@ -42,6 +43,13 @@ type statusEventStartControlBody struct {
 
 type statusEventStopControlBody struct {
 	ActorId uint32 `json:"actorId"`
+}
+
+type statusEventDamagedBody struct {
+	X             int16         `json:"x"`
+	Y             int16         `json:"y"`
+	ActorId       uint32        `json:"actorId"`
+	DamageEntries []damageEntry `json:"damageEntries"`
 }
 
 type statusEventKilledBody struct {
